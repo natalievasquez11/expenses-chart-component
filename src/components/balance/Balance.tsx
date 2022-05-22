d1d1ac8d106681c1c7f6937c7a2369b3ed7ae46a
Natalie Vasquez <natalievasquez11@gmail.com>
2022-05-22 17:02:51 -0500

    balance component styling

--- src/components/balance/Balance.tsx
+++ src/components/balance/Balance.tsx
@@ -3,7 +3,15 @@ import './balance.scss';
 
 const Balance = () => {
   return (
-    <div className='balance'>Balance</div>
+    <div className='balance'>
+      <span>
+        <p className='balance--text balance--title'>My Balance</p>
+        <p className='balance--text balance--total'>$921.48</p>
+      </span>
+      <span>
+        <img className='balance--logo' src='logo.svg' alt='logo'/>
+      </span>
+    </div>
   )
 };
 
